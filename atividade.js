40ef13c0758988844485adef63caafe3381e79d6
Samuel Vinícius da Luz <63885675+SamuelVLuz@users.noreply.github.com>
2024-11-25 20:23:30 -0300

    Add files via upload

---
+++ atividade.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+    // function calcularQuantidadeDeProdutos() {
+    //     quant = $("#produtos tbody tr").length;
+    //     $("#quant").text(quant);
+    // }
+
+    $("#salvar").click(function(){
+        nome = $("#nome").val();
+        cor = $("#cor").val();
+        tamanho = $("#tamanho").val();
+        quantidade = $("#quantidade").val();
+        imagem = $("#imagem").val();
+        nome_arq = imagem.substring(12);
+        $("#produtos").append(
+            "<ul class='produto' style='border: solid black 1px;'>" +
+                "<li class='image'>" +  
+                    "<img src='"+nome_arq+"'>" +
+                "</li>" +   
+                "<li class='nome'>" + nome + "</li>" +
+                "<li class='cor'>" + cor + "</li>" +
+                "<li class='tamanho'>" + tamanho + "</li>" + 
+                "<li class='quantidade'>" + quantidade + "</li>" + 
+                "<li><a href=''>Excluir</a></li>" +
+            "</ul>"
+        );
+    });
+
+    $("#excluirtudo").click(function(){
+        if (confirm("Deseja realmente excluir todos os dados?")) {
+            $("#produtos tbody").empty();
+        }
+    });
+
+    $("#produtos").on("click", "a", function(event){
+        event.preventDefault();
+        $(this).closest("ul").remove();
+    });
+});
